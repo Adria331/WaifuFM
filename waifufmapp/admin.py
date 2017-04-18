5ed5371b1b8d9d6f953c364de5f2df365ef9575a
Adria Bonet <adria331133@gmail.com>
2017-04-18 17:52:38 +0200

    Admin add modify delete

--- waifufmapp/admin.py
+++ waifufmapp/admin.py
@@ -3,4 +3,6 @@ from waifufmapp.models import *
 # Register your models here.
 
 admin.site.register(Review)
-
+admin.site.register(Song)
+admin.site.register(Genre)
+admin.site.register(Artist)
--- waifufmapp/models.py
+++ waifufmapp/models.py
@@ -24,7 +24,7 @@ class Song(models.Model):
     author = models.ForeignKey(Artist)
 
     def __unicode__(self):
-        return self.name + " by " + str(self.author)
+        return self.name + ", by " + str(self.author)
 
 class Album(models.Model):
     name = models.TextField()
@@ -32,17 +32,17 @@ class Album(models.Model):
     area = models.TextField()
     year = models.IntegerField(max_length = 4)
     author = models.ForeignKey(Artist)
-    Genre 
+    Genre = models.ForeignKey(Genre)
 
     def __unicode__(self):
-        return self.name + " by " + str(self.author)
+        return self.name + ", by " + str(self.author)
 
 
 class Review(models.Model):
-    RATING_CHOICES = ((1, 'one'), (2, 'two'), (3, 'three'), (4, 'four'), (5, 'five'))
+    RATING_CHOICES = ((1, '*'), (2, '**'), (3, '***'), (4, '****'), (5, '*****'))
     song = models.ForeignKey(Song)
     data = models.DateField()
     rating = models.PositiveSmallIntegerField("", blank = False, choices = RATING_CHOICES)
 
     def __unicode__(self):
-        return self.song + ": " + str(self.rating)
+        return self.song.name + ": " + str(self.rating)
